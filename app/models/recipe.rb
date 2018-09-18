521ed6fdef5e411b1e421f5f05a0b1b6c9152213
Joao Almeida <joaorsalmeida@gmail.com>
2018-09-18 19:27:49 -0300

    Add photo to recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,8 @@
 class Recipe < ApplicationRecord
+  has_attached_file :photo
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/,
+                                     message: 'Tipo da imagem não é válido'
+
   belongs_to :recipe_type
   belongs_to :cuisine
 
